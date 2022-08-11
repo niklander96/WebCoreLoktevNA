4e278a76f2758a3756e1930f02c35fe33bcca603
niklander96 <96373865+niklander96@users.noreply.github.com>
2022-08-11 23:05:10 +0300

    Add files via upload

--- Макет 1.5/javascript0.js
+++ Макет 1.5/javascript0.js
@@ -1,36 +1,43 @@
-const swiper = new Swiper('.swiper', {
+let slider = document.querySelector('.swiper');
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  slideToClickedSlide: true,
-  slidesPerView: 'auto',
-});
+let display = document.documentElement.clientWidth;
+  if (display <= '768') {
 
+    const swiper = new Swiper('.swiper', {
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slideToClickedSlide: true,
+      slidesPerView: 'auto',
+
+    });
+  }
+  else {
+    slider.classList.remove('swiper');
+  }
 
 let more = document.querySelectorAll('.slider__list_item_hidden');
 let btn = document.querySelector('.show-all__button');
-
-
-btn.addEventListener("click", function (e) {
+btn.addEventListener("click", function () {
   for (let i = 0; i < more.length; i++) {
     let item = more[i];
     if (item.style.display === "none") {
-      item.style.display = "inline-block";
-      btn.style.justifyContent = "flex-start";
-      btn.style.paddingLeft = "35px";
-      btn.style.backgroundImage = 'url(./img/icon/icon180.svg)'
+      item.style.display = "flex";
+      btn.classList.add('open__all');
       btn.textContent = "Скрыть";
     } else {
       item.style.display = "none";
       btn.textContent = "Показать все";
-      btn.style.justifyContent = "flex-end";
-      btn.style.backgroundImage = 'url(./img/icon/icon.svg)'
+      btn.classList.remove('open__all');
     }
   }
 
-})
+});
+
+
+
 
 
 
